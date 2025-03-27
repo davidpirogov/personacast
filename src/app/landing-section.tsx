e4b4b3690401a98c40fe906e5c2d12671744fa16
davidpirogov <580286+davidpirogov@users.noreply.github.com>
2025-03-27 09:42:13 +0000

    Remove deprecated components and providers
    - Deleted unused components and providers including landing-section, menu, root-provider, site-settings, session-check, various data tables, dialogs, forms, and UI elements to streamline the codebase.
    - This cleanup enhances maintainability by eliminating redundancy and focusing on active components.
    - Ensured that all related references and imports were also removed to maintain consistency across the application.

--- src/app/landing-section.tsx
+++
@@ -1,3 +0,0 @@
-export default function LandingPageSection() {
-    return <div>LandingPage</div>;
-}
--- src/components/ui/data-table/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./data-table";
-export * from "./cells";
-export * from "./types"; 
